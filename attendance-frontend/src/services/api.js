e053519ee1c4a18b218210446f42dc2a7050c341
Thomas jose <thomasjose22@gmail.com>
2024-12-21 16:32:07 +0530

    Update api.js

--- attendance-frontend/src/services/api.js
+++ attendance-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://simple-attendance-system.onrender.com/api';
+const API_URL = 'https://attendance-web-3ikt.onrender.com/api';
 
 // Create axios instance with base configuration
 const api = axios.create({
